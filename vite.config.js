b6ca4390df7e6301f46dde7f15a4f3de8725a1df
mushfiq <mushfiqxrabbi@gmail.com>
2023-04-13 08:21:35 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./react-authentication-using-firebase-auth/",
+  base: "/test-app/",
   plugins: [react()],
 });
