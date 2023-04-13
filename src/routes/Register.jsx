942035b529690a7d4004319776ff0e79da2fdfde
mushfiq <mushfiqxrabbi@gmail.com>
2023-04-13 09:11:59 +0600

    deploy

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -37,7 +37,7 @@ export default function Register() {
 
   return (
     <>
-      <div className="container col-md-6 my-5">
+      <div className="container col-md-6">
         <div
           className={`alert alert-danger alert-dismissible fade ${
             showAlert ? "show" : "hide"
